d15f58042e4cc2e65be3e7c13ff1887b277bd003
jpnodev <jpnodev@gmail.com>
2026-06-19 19:27:57 +0200

    update: new function FUN_8004634c

--- ld/SCES_008.68.d
+++ ld/SCES_008.68.d
@@ -5,6 +5,7 @@ build/SCES_008.68.elf: \
     build/asm/SCES_008.68/before_main.s.o \
     build/asm/SCES_008.68/main.s.o \
     build/asm/SCES_008.68/after_main.s.o \
+    build/src/SCES_008.68/FUN_8004634c.c.o \
     build/src/SCES_008.68/ff7_memset.c.o \
     build/asm/SCES_008.68/after_ff7_memset.s.o \
     build/assets/SCES_008.68/main_data.bin.o
@@ -14,7 +15,8 @@ build/asm/SCES_008.68/start.s.o:
 build/asm/SCES_008.68/before_main.s.o:
 build/asm/SCES_008.68/main.s.o:
 build/asm/SCES_008.68/after_main.s.o:
+build/src/SCES_008.68/FUN_8004634c.c.o:
 build/src/SCES_008.68/ff7_memset.c.o:
 build/asm/SCES_008.68/after_ff7_memset.s.o:
 build/assets/SCES_008.68/main_data.bin.o:
--include build/assets/SCES_008.68/psx_header.bin.d build/asm/SCES_008.68/before_start.s.d build/asm/SCES_008.68/start.s.d build/asm/SCES_008.68/before_main.s.d build/asm/SCES_008.68/main.s.d build/asm/SCES_008.68/after_main.s.d build/src/SCES_008.68/ff7_memset.c.d build/asm/SCES_008.68/after_ff7_memset.s.d build/assets/SCES_008.68/main_data.bin.d
+-include build/assets/SCES_008.68/psx_header.bin.d build/asm/SCES_008.68/before_start.s.d build/asm/SCES_008.68/start.s.d build/asm/SCES_008.68/before_main.s.d build/asm/SCES_008.68/main.s.d build/asm/SCES_008.68/after_main.s.d build/src/SCES_008.68/FUN_8004634c.c.d build/src/SCES_008.68/ff7_memset.c.d build/asm/SCES_008.68/after_ff7_memset.s.d build/assets/SCES_008.68/main_data.bin.d
