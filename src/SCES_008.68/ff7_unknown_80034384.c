a7c9d7e9a3bfbae9f956ba9992af453aea222c42
jpnodev <jpnodev@gmail.com>
2026-06-23 19:09:11 +0200

    Rename func_8003FAB0, func_80041C4C, func_80041D54 to CdSearchFile, CdRead, CdReadSync

--- src/SCES_008.68/ff7_unknown_80034384.c
+++ src/SCES_008.68/ff7_unknown_80034384.c
@@ -1,8 +1,8 @@
 extern int func_80034B50(void);
-extern int func_8003FAB0(char*, const char*);
+extern int CdSearchFile(char*, const char*);
 extern void CdControlB(int, char*, int);
-extern void func_80041C4C(int, void*, int);
-extern int func_80041D54(int, int);
+extern void CdRead(int, void*, int);
+extern int CdReadSync(int, int);
 
 extern char D_80069A3C;
 extern unsigned char D_80069A43;
@@ -18,16 +18,16 @@ int ff7_unknown_80034384(void) {
     } while (iVar1 != 0);
 
     do {
-        iVar1 = func_8003FAB0(auStack_20, D_80010438);
+        iVar1 = CdSearchFile(auStack_20, D_80010438);
         if (iVar1 <= 0) {
             if (iVar1 >= -1) {
                 return -1;
             }
         }
         CdControlB(2, auStack_20, 0);
-        func_80041C4C(1, &D_80069A3C, 0x80);
+        CdRead(1, &D_80069A3C, 0x80);
         do {
-            iVar1 = func_80041D54(1, 0);
+            iVar1 = CdReadSync(1, 0);
         } while (0 < iVar1);
     } while (iVar1 != 0);
 
--- src/SCES_008.68/ff7_unknown_80034F68.c
+++ src/SCES_008.68/ff7_unknown_80034F68.c
@@ -1,7 +1,7 @@
-extern int func_8003FAB0(char*, const char*);
+extern int CdSearchFile(char*, const char*);
 extern void CdControlB(int, char*, int);
-extern void func_80041C4C(int, void*, int);
-extern int func_80041D54(int, int);
+extern void CdRead(int, void*, int);
+extern int CdReadSync(int, int);
 
 extern const char D_8001049C[];
 extern char D_8009A308;
@@ -12,13 +12,13 @@ void ff7_unknown_80034F68(void) {
     
     do {
         do {
-            iVar1 = func_8003FAB0(auStack_20, D_8001049C);
+            iVar1 = CdSearchFile(auStack_20, D_8001049C);
         } while (iVar1 == 0);
         
         CdControlB(2, auStack_20, 0);
-        func_80041C4C(1, &D_8009A308, 0x80);
+        CdRead(1, &D_8009A308, 0x80);
         do {
-            iVar1 = func_80041D54(1, 0);
+            iVar1 = CdReadSync(1, 0);
         } while (0 < iVar1);
     } while (iVar1 != 0);
 }
